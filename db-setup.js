07a72dc2b4da3bcd5bd9a8d9781416dae5ff0979
Hunter Gatewood <hcg@mit.edu>
2016-01-14 00:24:06 -0500

    Get initial room text working

--- db-setup.js
+++ db-setup.js
@@ -2,8 +2,10 @@ var mongo = require('mongodb').MongoClient;
 var dbConnectionUrl = 'mongodb://localhost:27017/test';
 var collections = {};
 var fs = require('fs');
+var path = require('path');
 
-var defaultTextFilename = 'bootstrap-data/default-room-text.txt'
+//var defaultTextFilename = 'bootstrap-data/default-room-text.txt'
+var roomTextDir = 'bootstrap-data/room-text';
 
 mongo.connect(dbConnectionUrl, function (err, db) {
     if (err) {
@@ -15,18 +17,27 @@ mongo.connect(dbConnectionUrl, function (err, db) {
     collections.users = db.collection('users');
     collections.rooms = db.collection('rooms');
 
-    // Initial bootstrapped data
-    collections.rooms.drop()
-    fs.readFile(defaultTextFilename, 'utf8', function (err, text) {
-        if (err) {
-            console.log('Cannot read from: ' + defaultTextFilename);
-            return;
-        }
-        collections.rooms.insert({
-            _id: 'main-room',
-            text: text
-        });
-    });
+    //// Initial bootstrapped room data
+    //collections.rooms.drop();
+    //collections.rooms.insert({
+        //_id: 'main-room',
+        //contributions: []
+    //});
+    //var filenames = fs.readdirSync(roomTextDir);
+    //var filePath;
+    //var filename;
+    //var text;
+    //for (var idx = 0; idx < filenames.length; idx++) {
+        //filename = filenames[idx];
+        //filePath = path.join(roomTextDir, filename);
+        //text = fs.readFileSync(filePath, 'utf8');
+        ////console.log(filename);
+        ////console.log(text);
+        //collections.rooms.update(
+            //{_id: 'main-room'},
+            //{$push: {contributions: {user: filename, text: text}}}
+        //);
+    //}
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db-setup.js');
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+
+var roomTextDir = 'bootstrap-data/room-text';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,15 +14,52 @@ router.get('/', function(req, res, next) {
 
 /* GET room page. */
 router.get('/room', function(req, res, next) {
+
+    // Initial bootstrapped room data
+    db.rooms.remove();
+    db.rooms.insert({
+        _id: 'main-room',
+        contributions: []
+    });
+    var filenames = fs.readdirSync(roomTextDir);
+    var filePath;
+    var filename;
+    var text;
+    for (var idx = 0; idx < filenames.length; idx++) {
+        filename = filenames[idx];
+        filePath = path.join(roomTextDir, filename);
+        text = fs.readFileSync(filePath, 'utf8');
+        //console.log(filename);
+        //console.log(text);
+        db.rooms.update(
+            {_id: 'main-room'},
+            {$push: {contributions: {user: filename, text: text}}},
+            function (err) {}
+        );
+    }
+
+    var roomName = req.query.roomname;
+    console.log('********');
+    console.log(roomName);
     db.rooms.find({}).toArray(function (err, rooms) {
-        var roomTexts = {};
+        console.log('foob');
+        console.log(rooms);
+    });
+    console.log('Routing...');
+    db.rooms.find({_id: roomName}).toArray(function (err, rooms) {
+        console.log('Finding...');
+        var numRoomsFound = 0;
+        console.log(rooms.length);
         rooms.forEach(function (room) {
-            roomTexts[room._id] = room.text;
-        });
-        res.render('room', {
-            title: 'Main Room',
-            storyText: roomTexts['main-room'],
-            userTurn: false
+            console.log('Iterating...');
+            numRoomsFound++;
+            assert(numRoomsFound == 1);
+            console.log('Rendering...');
+            res.render('room', {
+                title: roomName,
+                contributions: room.contributions,
+                userTurn: false
+            });
         });
     });
 });
