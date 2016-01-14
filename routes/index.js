653cc32cd0eade72fcc8f7677f8e9865650372a9
Rachel <simpsonr@mit.edu>
2016-01-14 17:26:46 -0500

    find a story page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Rolling Story' });
 });
 
+router.get('/find', function(req,res){
+	res.render('find', {title:'Find a Story!'});
+});
+
 module.exports = router;
