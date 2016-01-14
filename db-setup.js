2d41c6004db8c0fc1e4edb48b8d15d02be7b0504
Hunter Gatewood <hcg@mit.edu>
2016-01-14 00:52:58 -0500

    Clean up room page additions

--- db-setup.js
+++ db-setup.js
@@ -1,8 +1,6 @@
 var mongo = require('mongodb').MongoClient;
 var dbConnectionUrl = 'mongodb://localhost:27017/test';
 var collections = {};
-var fs = require('fs');
-var path = require('path');
 
 //var defaultTextFilename = 'bootstrap-data/default-room-text.txt'
 var roomTextDir = 'bootstrap-data/room-text';
@@ -16,28 +14,6 @@ mongo.connect(dbConnectionUrl, function (err, db) {
 
     collections.users = db.collection('users');
     collections.rooms = db.collection('rooms');
-
-    //// Initial bootstrapped room data
-    //collections.rooms.drop();
-    //collections.rooms.insert({
-        //_id: 'main-room',
-        //contributions: []
-    //});
-    //var filenames = fs.readdirSync(roomTextDir);
-    //var filePath;
-    //var filename;
-    //var text;
-    //for (var idx = 0; idx < filenames.length; idx++) {
-        //filename = filenames[idx];
-        //filePath = path.join(roomTextDir, filename);
-        //text = fs.readFileSync(filePath, 'utf8');
-        ////console.log(filename);
-        ////console.log(text);
-        //collections.rooms.update(
-            //{_id: 'main-room'},
-            //{$push: {contributions: {user: filename, text: text}}}
-        //);
-    //}
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db-setup.js');
 var assert = require('assert');
-var fs = require('fs');
-var path = require('path');
-
-var roomTextDir = 'bootstrap-data/room-text';
+var bootstrapSync = require('../lib/bootstrapSync.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,46 +12,15 @@ router.get('/', function(req, res, next) {
 /* GET room page. */
 router.get('/room', function(req, res, next) {
 
-    // Initial bootstrapped room data
-    db.rooms.remove();
-    db.rooms.insert({
-        _id: 'main-room',
-        contributions: []
-    });
-    var filenames = fs.readdirSync(roomTextDir);
-    var filePath;
-    var filename;
-    var text;
-    for (var idx = 0; idx < filenames.length; idx++) {
-        filename = filenames[idx];
-        filePath = path.join(roomTextDir, filename);
-        text = fs.readFileSync(filePath, 'utf8');
-        //console.log(filename);
-        //console.log(text);
-        db.rooms.update(
-            {_id: 'main-room'},
-            {$push: {contributions: {user: filename, text: text}}},
-            function (err) {}
-        );
-    }
+    bootstrapSync.reloadRoomData();
 
+    // Serve room page with appropriate data
     var roomName = req.query.roomname;
-    console.log('********');
-    console.log(roomName);
-    db.rooms.find({}).toArray(function (err, rooms) {
-        console.log('foob');
-        console.log(rooms);
-    });
-    console.log('Routing...');
     db.rooms.find({_id: roomName}).toArray(function (err, rooms) {
-        console.log('Finding...');
         var numRoomsFound = 0;
-        console.log(rooms.length);
         rooms.forEach(function (room) {
-            console.log('Iterating...');
             numRoomsFound++;
             assert(numRoomsFound == 1);
-            console.log('Rendering...');
             res.render('room', {
                 title: roomName,
                 contributions: room.contributions,
