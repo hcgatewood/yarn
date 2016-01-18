40b1714172155ca40d53ac62afd000258f392832
Rachel <simpsonr@mit.edu>
2016-01-18 16:44:50 -0500

    started overlay, still in progress

--- public/javascripts/find.js
+++ public/javascripts/find.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
 	window.onresize = function(event) {
       var win = $(this); //this = window
-      if (win.height() >= 820) { 
-      		$('.footer').css({
-            position: 'static',
-            bottom: 'auto',
-            left: 'auto'}
-      if (win.width() >= 1280) { /* ... */ }
-	}
-});
+ &});
