cebe767686a14703426c697baf45f8d937333997
Hunter Gatewood <hcg@mit.edu>
2016-01-22 03:05:54 -0500

    Add dynamic auth redirect

--- config/auth.js
+++ config/auth.js
@@ -1,18 +1,19 @@
 // config/auth.js
 
+var rootDir = process.env.APP_URL || 'http://localhost:3000';
+
 // expose our config directly to our application using module.exports
 module.exports = {
-
     'facebookAuth' : {
         'clientID'      : '1019147024815879', // your App ID
         'clientSecret'  : '1bc317215eb534ac068cb44416f578a6', // your App Secret
-        'callbackURL'   : '/auth/facebook/callback'
+        'callbackURL'   : rootDir + '/auth/facebook/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '322693589157-h8q2jv3epfe6fcs1lf3llcsn9v38f1sm.apps.googleusercontent.com',
         'clientSecret'  : 'W4ZNTRjjPJMIh_DaYo2OAD0e',
-        'callbackURL'   : '/auth/google/callback'
+        'callbackURL'   : rootDir + '/auth/google/callback'
     }
 
 };
