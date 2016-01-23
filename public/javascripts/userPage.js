463b5e1094d2ce3dab87db8b8d90e6efeae53f52
Rachel <simpsonr@mit.edu>
2016-01-23 18:04:48 -0500

    Toggle follow button on user page

--- public/javascripts/userPage.js
+++ public/javascripts/userPage.js
@@ -11,5 +11,18 @@ $(document).ready(function() {
  
         e.preventDefault();
     });
+
+    $('.follow_btn').click(function(){
+    var $this = $(this);
+    $this.toggleClass('.follow-toggle btn-primary');
+    if(!$this.hasClass('.follow-toggle')){
+        $this.text('Follow');
+        $this.removeClass('btn-primary2')         
+    } else {
+        $this.text('Following');
+        $this.addClass('btn-primary2')
+
+    }
+});
 });
 
